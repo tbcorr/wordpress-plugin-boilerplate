7532a7bda6927f99342d4e26705dbf5044ff56a3
tim <tim@launchdm.com>
2015-12-09 20:23:15 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Wordpress Plugin Boilerplate
+Basically a starter file for a WordPress plugin used frequently enough that I decided to put it on GitHub cause why not?
---
+++ wordpress-plugin-boilerplate.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Plugin Name: WordPress Plugin Boilerplate.
+ * Description: WordPress plugin boilerplate.
+ * Version: 0.1.0
+ * Author: tbcorr
+ * Author URI: http://www.timcorr.com
+ */
+
+namespace Plugin;
+
+class Plugin {
+
+    private static $instance = null;
+
+    private function __construct(){
+
+        $this->setup_autoload();
+
+        $this->setup_actions();
+        $this->setup_filters();
+    }
+
+    public static function instance(){
+
+        if( self::$instance === null ){
+
+            self::$instance = new Plugin();
+        }
+
+        return self::$instance;
+    }
+
+    public static function get_plugin_dir(){
+
+        return plugin_dir_path( __FILE__ );
+    }
+
+    public static function get_plugin_url(){
+
+        return plugin_dir_url( __FILE__ );
+    }
+
+    public static function get_vendor_dir(){
+
+        return self::get_plugin_dir() . 'vendor/';
+    }
+
+    private function setup_autoload(){
+
+        $autoloader = self::get_vendor_dir() . 'autoload.php';
+
+        if( file_exists( $autoloader ) ){
+
+            require_once( $autoloader );
+        }
+    }
+
+    private function setup_actions(){
+
+    }
+
+    private function setup_filters(){
+
+    }
+}
+
+RocketRewrites::instance();
